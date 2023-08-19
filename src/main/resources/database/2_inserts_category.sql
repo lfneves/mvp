d5cd221202b0962abc601eac793df3220a9d20c9
lfneves <lfneves.dg@gmail.com>
2023-08-18 23:44:45 -0300

    feat: add 2_inserts_category.sql new script

---
+++ src/main/resources/database/2_inserts_category.sql
@@ -0,0 +1,15 @@
+INSERT INTO public.tb_category (name, description, dh_insert)
+VALUES ('Bebidas', 'Consumível líquido que possa ser comprado para consumo, como Água, Refrigerante, Cerveja entre outros.', now())
+ON CONFLICT DO NOTHING;
+
+INSERT INTO public.tb_category (name, description, dh_insert)
+VALUES ('Sobremesa', 'Algum doce, pode incluir bolos, tortas, sorvetes, pudins, entre outros.', now())
+ON CONFLICT DO NOTHING;
+
+INSERT INTO public.tb_category (name, description, dh_insert)
+VALUES ('Lanche', 'Refeição pronta para consumo que possa ser comprada como Hambuguer, Pizza, Sanduiche entre outros.', now())
+ON CONFLICT DO NOTHING;
+
+INSERT INTO public.tb_category (name, description, dh_insert)
+VALUES ('Acompanhamento', 'Todo e qualquer alimento de pequena média proporção que possa ser consumido com outras refeições.', now())
+ON CONFLICT DO NOTHING;
